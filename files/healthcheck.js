07462cda4608c00208242b1137be98aab9354620
jrofurtado <jrofurtado@gmail.com>
2020-01-22 17:41:16 -0100

    bug fix healthcheck

--- files/healthcheck.js
+++ files/healthcheck.js
@@ -1,21 +1,21 @@
 var http = require("http");
 
-var options = {  
-    host: 'localhost',
-    port: '3000',
-    path: '/api/message',
-    timeout: 2000
-};
+var options = {
+  host: 'localhost',
+  port: '3000',
+  path: '/api/ping',
+  timeout: 2000
+}
 
 var request = http.request(options, (res) => {
-    if (res.statusCode != 200) {  
-        process.exit(1);
-    }
-    process.exit(0);
-});
+  if (res.statusCode != 200) {
+    process.exit(1)
+  }
+  process.exit(0)
+})
 
-request.on('error', function(err) {  
-    process.exit(1);
-});
+request.on('error', function(err) {
+  process.exit(1)
+})
 
-request.end();
+request.end()
